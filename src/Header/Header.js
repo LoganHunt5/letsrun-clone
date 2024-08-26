4673ef91883d4f90b0468d36cb833f07cad94e89
Logan <l.b.hunt5@gmail.com>
2024-08-26 15:56:58 -0700

    drop

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,12 @@
 import './Header.css'
 import SearchMagGrey from './pics/SearchMagGrey.png';
 import SearchMagYellow from './pics/SearchMagYellow.png';
+import DropArrow from './pics/DropArrow.png'
+import DropArrowHover from './pics/DropArrowHover.png'
 
 function Link({ which }){
-    const name=which.name !== "World Famous Message Board" ? which.name :
-        <div style={{ color: `rgb(251, 196, 57)`}}>
-            {which.name}
-        </div>;
     return(
-        <a className={which.class} href={which.link}>{name}</a>
+        <a className={which.class} href={which.link}>{which.name}</a>
     );
 }
 
@@ -25,8 +23,8 @@ function SearchBar(){
 function ShowBar(){
     const BarWrap = document.getElementById('BarWrap');
     if (BarWrap){
-        if(BarWrap.style.width !== '200px'){
-            BarWrap.style.width = '200px';
+        if(BarWrap.style.width === '0px'){
+            BarWrap.style.width = '250px';
         }else{
             BarWrap.style.width = '0px';
         }
@@ -50,11 +48,45 @@ function SearchMag(){
     );
 }
 
-/*function DropDown(){
+function DropItem({which}){
+    return(
+        <li className={which.class}><a  href={which.link}>{which.name}</a></li> 
+    )
+}
 
-}*/
+function DropMenu({about}){
+    const links = [];
+    about.forEach((link)=> {
+        links.push(
+            <DropItem
+                which={link}
+                key={link.name} />
+        );
+    });
+    return (
+        <div className="DropMenu">
+            <ul>
+                {links}
+            </ul>
+        </div>
+    ); 
+}
 
-function InternalHeader({ links }){
+function DropDown({about}){
+    return(
+        <div className="DropWrap">
+            <div className="Drop">
+                <img src={DropArrow} />
+            </div>
+            <div className="DropHover">
+                <img src={DropArrowHover} />
+            </div>
+            <DropMenu about={about} />
+        </div>
+    );
+}
+
+function InternalHeader({ links, about }){
     const parts = [];
     links.forEach((link) => {
         parts.push(
@@ -66,22 +98,25 @@ function InternalHeader({ links }){
     return(
         <div className="Header"> 
             {parts}
+            <DropDown about={about} />
             <SearchMag />
         </div>
     );
 };
 
 const LINKS = [
-    {name: "World Famous Message Board", link: "nothing.com", class:"Link"},
-    {name: "Better Running Shoes", link: "nothing.com", class:"Link"},
-    {name: "Podcast", link: "nothing.com", class:"Link"},
-    {name: "About", link: "nothing.com", class:"Link"},
-    {name: "Shop", link: "nothing.com", class:"Link"},
-    {name: "Subscribe", link: "nothing.com", class:"Link"},
-    {name: "Sign In", link: "nothing.com", class:"LinkSignIn"},
-    {name: "FAKERUN.COM", link: "nothing.com", class:"FakeRun"}
+    {name: "Blog", link: "nothing", class:"Blog"},
+    {name: "Portfolio", link: "nothing", class:"Link"},
+    {name: "Podcast", link: "nothing", class:"Link"},
+    {name: "About", link: "nothing", class:"Link"},
+    {name: "Sign In", link: "nothing", class:"LinkSignIn"}
+    /*{name: "FAKERUN.COM", link: "nothing", class:"FakeRun"}*/
 ];
+const ABOUT = [
+    {name: "FAQ", link: "nothing.com", class:"AboutLink"},
+    {name: "Test", link: "nothing.com", class:"AboutLink"},
+]
 
 export default function Header() {
-    return <InternalHeader links={LINKS} />;
+    return <InternalHeader links={LINKS} about={ABOUT} />;
 };
